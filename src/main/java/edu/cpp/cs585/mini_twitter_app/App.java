869e3a42a968134f88316183bc547972089b89ea
dlin345 <dlin345@gmail.com>
2016-10-28 23:20:03 -0700

    init commit

---
+++ src/main/java/edu/cpp/cs585/mini_twitter_app/App.java
@@ -0,0 +1,13 @@
+package edu.cpp.cs585.mini_twitter_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
